35849fee8bf52fe947b8f4247326d14c773724bb
Marcelo Plínio <mplmdswork@gmail.com>
2025-04-05 19:23:33 -0300

    refactor: Update API base URL to follow REST conventions

--- api.js
+++ api.js
@@ -4,9 +4,13 @@ const port = 3000;
 const router = require("./routers/index");
 const conn = require("./database/connection");
 const tables = require("./database/tables");
+const carRoute = require('./routers/carsRoute')
 
 router(app, express);
 tables.init(conn);
+
+
+app.use("/api/v1", carRoute);
 
 app.listen(3000, (error) => {
   if (error) {
